c5baa4c72d7494a691bc8b19b3aeff32f48edcf2
Alan <salan2000@wp.pl>
2021-11-19 19:04:06 +0100

    Modification: modified Help function

--- condict.sh
+++ condict.sh
@@ -8,12 +8,14 @@ set -u
 Help(){
     echo "Check definition of given word"
     echo
-    echo "Usage: condict [arguments]  [word ..]         get definition and an example of word"
+    echo "Usage: condict [-h|s|p|P]  [word ..]         get definition and an example of word"
     echo
     echo "Arguments:"
+    echo "  -h      show this help"
     echo "  -s      list 5 synonyms"
     echo "  -p      part of speech"
     echo "  -P      phonetic"
+    echo "  -V      show version"
     echo
     echo "DM me if you've some other features in mind -> El Patron Salan#6684"
     echo
